b9e9716e669e1d59760df72ebe8853ae497baa55
Abosede Kikelomo Olawale <abosedekikelomo408@gmail.com>
2024-01-31 04:16:15 +0100

    Added JWT auth as apps default

---
+++ 750HrsTracker/Extensions/CustomAuthenticationExtension.cs
@@ -0,0 +1,79 @@
+﻿using _750HrsTracker.Helpers;
+using _750HrsTracker.Models.Misc;
+using _750HrsTracker.Repositories.Interfaces;
+using _750HrsTracker.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace _750HrsTracker.Extensions
+{
+    public static class CustomAuthenticationExtension
+    {
+        public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, AppSettings appSettings)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            })
+               .AddJwtBearer(options =>
+               {
+                   options.Events = new JwtBearerEvents
+                   {
+
+                       OnTokenValidated = context =>
+                       {
+                           var userService = context.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+                           var principal = context.Principal!;
+
+                           //set up authorization response
+                           var response = context.Response;
+                           ErrorModel rhe = new ErrorModel();
+                           response.ContentType = "application/json";
+
+
+                           if (principal.HasClaim(c => c.Type == "Id") && principal.Identity!.IsAuthenticated)
+                           {
+
+                               var userId = Guid.Parse(principal.Claims.First(x => x.Type == "Id").Value);
+                               var user = userService.GetUserAsync(userId).Result;
+                               if (user == null)
+                               {
+                                   // return unauthorized if user no longer exists
+                                   context.Fail("Unathorized");
+                                   rhe.message = "Unauthorized";
+                                   response.WriteAsync(JsonConvert.SerializeObject(rhe));
+                                   return Task.CompletedTask;
+                               }
+                               context.HttpContext.Request.Headers["TeamId"] = user!.DefaultTeamId!;
+                               context.Success();
+                               return Task.CompletedTask;
+                           }
+
+                           context.Fail("Invalid token");
+                           rhe.message = "Invalid token";
+                           response.WriteAsync(JsonConvert.SerializeObject(rhe));
+                           return Task.CompletedTask;
+                       }
+
+                   };
+                   options.RequireHttpsMetadata = false;
+                   options.SaveToken = true;
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = false,
+                       ValidateAudience = false,
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(appSettings.JwtSecret!)),
+                       ValidateIssuerSigningKey = true,
+                       ClockSkew = TimeSpan.Zero
+                   };
+               });
+
+            return services;
+        }
+    }
+}
