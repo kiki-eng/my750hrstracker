cdec5a313f0d3c30bf8592f9e8dc5e5626b91d48
Abosede Kikelomo Olawale <abosedekikelomo408@gmail.com>
2024-02-03 19:13:20 +0100

    added document upload response

---
+++ 750HrsTracker/DTOs/Responses/DocumentUploadResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace _750HrsTracker.DTOs.Responses
+{
+    public class DocumentUploadResponse
+    {
+        public string? FileAbsoluteUri { get; set; }
+        public string? DirectoryName { get; set; }
+
+    }
+}
