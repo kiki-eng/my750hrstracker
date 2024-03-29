7409c66fc0526fff0febc7faac938b5da40e31d4
Abosede Kikelomo Olawale <abosedekikelomo408@gmail.com>
2024-03-30 00:53:49 +0100

    updated mapping profile for activity log

--- 750HrsTracker/AutoMapperProfiles/ActivityLogActivityProfiles.cs
+++ 750HrsTracker/AutoMapperProfiles/ActivityLogActivityProfiles.cs
@@ -15,6 +15,7 @@ namespace _750HrsTracker.AutoMapperProfiles
 
 
             CreateMap<AddActivityLogRequest, ActivityLog>();
+            CreateMap<BaseAddActivityLogRequest, ActivityLog>();
             CreateMap<ActivityLog, GetActivityLogResponse>();
         }
     }
