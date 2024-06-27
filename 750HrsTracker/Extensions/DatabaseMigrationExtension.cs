c4caaa9307cb09003f7b01d6bb4db80691e16521
Abosede Kikelomo Olawale <abosedekikelomo408@gmail.com>
2024-06-27 23:26:05 +0100

    update db seed for subscriptions

--- 750HrsTracker/Extensions/DatabaseMigrationExtension.cs
+++ 750HrsTracker/Extensions/DatabaseMigrationExtension.cs
@@ -37,7 +37,7 @@ namespace _750HrsTracker.Extensions
                     await Persistence.Seeds.DefaultLogData.SeedDefaultCatgoriesAsync(context);
                     await Persistence.Seeds.DefaultLogData.SeedDefaultLogActivityAsync(context);
                     await Persistence.Seeds.DefaultLogData.SeedDefaultStrLogActivityAsync(context);
-                    //await Persistence.Seeds.UpdateSubscriptionsPriceId.SeedSubscriptionPricesAsync(context, appSettings);
+                    await Persistence.Seeds.UpdateSubscriptionsPriceId.SeedSubscriptionPricesAsync(context, appSettings);
 
                     logger.LogInformation("Application starting ...");
 
