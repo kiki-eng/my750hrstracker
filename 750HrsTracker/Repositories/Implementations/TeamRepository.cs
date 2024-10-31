8e1185a4c581e1f3bb934a23742b663db898ab6a
Abosede Kikelomo Olawale <abosedekikelomo408@gmail.com>
2024-10-31 16:57:33 +0100

    updated roles fetch

--- 750HrsTracker/Repositories/Implementations/TeamRepository.cs
+++ 750HrsTracker/Repositories/Implementations/TeamRepository.cs
@@ -77,11 +77,11 @@ namespace _750HrsTracker.Repositories.Implementations
 
             var team = await _context.Teams.FirstOrDefaultAsync(m => m.Id == teamId) ?? throw new KeyNotFoundException("Invalid team");
 
-            //var defaultRoles = await _context.Roles.Where(r => r.Name.ToLower() != Roles.Owner.ToString().ToLower() && r.Default).ToListAsync();
+            var defaultRoles = await _context.Roles.ToListAsync();
 
             var customRoles = await _context.Roles.Where(r => r.TeamId.Equals(team.Id) && !r.Default).ToListAsync();
 
-            roles = customRoles;
+            roles = defaultRoles.Union(customRoles).ToList();
 
             return roles;
         }
